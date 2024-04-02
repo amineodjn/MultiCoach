396bc0f901cbee2e55563b1835a7c23ea8cb9b16
Amine Oudjana <amine.oudjana@withsecure.com>
2024-04-02 09:19:16 +0200

    added toast

---
+++ src/components/toast.vue
@@ -0,0 +1,78 @@
+<template>
+  <div v-if="success" id="toast-success" 
+  class="toast flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+  :class="{ show : success }"
+  @animationend="resetSuccess"
+  role="alert">
+    <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+      </svg>
+      <span class="sr-only">Check icon</span>
+    </div>
+    <div class="ms-3 text-sm font-normal">Saved successfully.</div>
+    <button 
+      type="button" 
+      class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" 
+      @click="success = false"
+      data-dismiss-target="#toast-success" 
+      aria-label="Close">
+        <span class="sr-only">Close</span>
+        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+        </svg>
+    </button>
+  </div>
+</template>
+
+<script setup>
+
+
+const props = defineProps({
+  success: Boolean
+
+});
+
+const resetSuccess = (event) => {
+  if (event.animationName === 'slideOutRight-6baabe41') {
+      success.value = false;
+  }
+};
+</script>
+
+<style scoped>
+.toast {
+    position: fixed;
+    top: 20px;
+    right: 20px;
+    z-index: 1023; 
+    animation: slideInRight 0.3s 
+            ease-in-out forwards, 
+            slideOutRight 0.5s ease-in-out 
+            forwards 3s; 
+    transform: translateX(110%);
+}
+
+.toast.show {
+    transform: translateX(0);
+}
+
+@keyframes slideInRight { 
+  from {
+    transform: translateX(110%);
+  }
+  to {
+    transform: translateX(0);
+  }
+} 
+  
+@keyframes slideOutRight { 
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(110%);
+  }
+} 
+
+</style>
