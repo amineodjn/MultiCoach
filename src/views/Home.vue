a5ad2a25d4444834b949116ba4d8edd0f1de3c25
Amine Oudjana <amine.oudjana@withsecure.com>
2024-04-22 09:03:21 +0200

    fixed the display of filterdopdown and added filtering by experience feature

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
               @click="dropdownToggle"
               data-dropdown-toggle="filterDropdown" class="w-auto md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none md:bg-white rounded-lg md:border md:border-gray-200 md:hover:bg-gray-100 hover:text-primary-700   md:dark:focus:ring-gray-700 md:dark:bg-gray-800 dark:text-gray-400 md:dark:border-gray-600 dark:hover:text-white md:dark:hover:bg-gray-700" type="button">
               <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="h-4 w-4 mr-2" viewbox="0 0 20 20" fill="currentColor">
-                  <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
+                <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
               </svg>
               <svg class="-mr-1 ml-1.5 w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                   <path clip-rule="evenodd" fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
@@ -17,7 +17,7 @@
             </button>
             <div id="filterDropdown" 
             :class="{'hidden' : !toggled}"
-            class="z-10 w-48 p-3 mt-3 bg-white rounded-lg shadow dark:bg-gray-700">
+            class="absolute top-[190px] z-10 w-48 p-3 mt-3 bg-white rounded-lg shadow dark:bg-gray-700">
                 <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">Experienced</h6>
                 <ul class="space-y-2 text-sm" aria-labelledby="filterDropdownButton">
                   <li v-for="exp in experiences" :key="exp" class="flex items-center">
@@ -26,7 +26,7 @@
                     v-model="toggle[exp]"  
                     true-value="yes"
                     false-value="no"
-                    @change="select"
+                    @change="filterExperiences(exp)"
                     class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
                     <label :for="exp" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">{{ exp }}</label>
                   </li>
@@ -34,15 +34,7 @@
             </div>
         </div>
         </div>
-        <div v-if="filteredUsers.length > 0" v-for="user in filteredUsers" :key="user.id">
-          <card 
-            :user="user" 
-            class="mt-2"
-            @book="console.log(user)"
-            @favorite="toggleFavorite"
-            />
-        </div>
-        <div v-if="!filteredUsers.length > 0" v-for="user in usersData" :key="user.id">
+        <div v-for="user in usersToShow" :key="user.id">
           <card 
             :user="user" 
             class="mt-2"
@@ -61,7 +53,7 @@
 </template>
 <script setup>
 import searchBox from '../components/searchbox.vue'
-import { ref, onMounted, reactive } from 'vue'
+import { ref, onMounted, reactive, computed } from 'vue'
 import { GoogleMap } from 'vue3-google-map';
 import card from '../components/card.vue'
 import users from '../users.js'
@@ -71,15 +63,20 @@ const filteredUsers = ref([])
 const center = ref({ lat: 52.00, lng: 20.00 })
 const toggle = ref({})
 const toggled = ref(false)
-const experiences = ref(['Weight loss', 'Strength', 'Squash', 'Spinning', 'Boxing'])
+const experiences = ref(['Personal Trainer', 'Yoga Instructor', 'Zumba Instructor', 'Spinning Instructor', 'Bodybuilding Coach'])
 const selectedExperiences = ref([])
 const isFavorite = ref(false)
+
+const usersToShow = computed(() => {
+  return filteredUsers.value.length > 0 ? filteredUsers.value : usersData;
+})
 
 const dropdownToggle = () => {
   toggled.value = !toggled.value
 }
 const select = () => {
   selectedExperiences.value  = Object.keys(toggle.value).filter(key => toggle.value[key] === 'yes');
+  console.log(selectedExperiences.value);
 }
 const toggleFavorite = (user) => {
   user.favorite = !user.favorite
@@ -89,6 +86,11 @@ const filterCities = (city) => {
 }
 const clearFilter = () => {
   filteredUsers.value = []
+}
+
+const filterExperiences = (experience) => {
+  selectedExperiences.value  = Object.keys(toggle.value).filter(key => toggle.value[key] === 'yes');
+  filteredUsers.value = usersData.filter((user) => selectedExperiences.value.includes(user.title))
 }
 
 </script>
