7a6ce9eef7870d8bf7e0613fe4e46836670a327e
Amine Oudjana <amine.oudjana@withsecure.com>
2024-07-20 20:27:36 +0200

    Added Offers to bookings page

--- src/components/bookingCard.vue
+++ src/components/bookingCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col border-gray-300 mt-5 rounded-lg bg-white shadow-sm p-4">
     <div class="flex justify-between items-center py-4 px-5">
-      <h2 class="text-2xl font-bold">Your Bookings</h2>
+      <h2 class="text-2xl font-bold">Classes</h2>
       <div class="flex items-center md:justify-end space-x-2">
         <div class="relative">
           <input v-model="searchTerm" type="text" placeholder="Search" class="pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:border-indigo-800">
@@ -17,14 +17,12 @@
           :trainingClass="Class" 
           :customWidth="'w-1/2'"
           :coachAccess="false"
-          :read-only="true"
-          @book="toggleModal"
-          @deleteClass="deleteClass" />
+          :read-only="true" />
       <emptyState v-if="displayedClasses.length === 0" />
     </div>
     <div v-if="displayedClasses.length > 2" class="text-center dark:border-neutral-70 hover:bg-gray-50">
       <a class="flex items-center text-blue-600 font-medium border-b text-sm leading-5 p-3 rounded-b-md space-x-1 justify-center  dark:text-indigo-500 dark:hover:text-indigo-600 dark:focus:bg-neutral-700"
-         @click="viewAllProjects">
+         @click="viewAllClasses">
         {{ showAllClasses ? 'Show less' : 'Show all' }}
         <svg class="flex-shrink-0  w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <path d="m9 18 6-6-6-6"></path>
@@ -32,22 +30,55 @@
       </a>          
     </div>
   </div>
+  <div class="flex flex-col border-gray-300 mt-5 rounded-lg bg-white shadow-sm p-4">
+    <div class="flex justify-between items-center py-4 px-5">
+      <h2 class="text-2xl font-bold">Offers</h2>
+      <div class="flex items-center md:justify-end space-x-2">
+        <div class="relative">
+          <input v-model="searchTerm" type="text" placeholder="Search" class="pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:border-indigo-800">
+          <svg class="w-5 h-5 text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/2 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+          </svg>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col border-gray-300 mt-5 rounded-lg bg-white shadow-sm">
+      <offersCard 
+          v-for="offer in displayedOffers" :key="offer.uid"
+          :offer="offer" 
+          :customWidth="'w-1/2'"
+          :coachAccess="false"
+          :read-only="true" />
+      <emptyState v-if="displayedOffers.length === 0" />
+    </div>
+    <div v-if="displayedOffers.length > 2" class="text-center dark:border-neutral-70 hover:bg-gray-50">
+      <a class="flex items-center text-blue-600 font-medium border-b text-sm leading-5 p-3 rounded-b-md space-x-1 justify-center dark:text-indigo-500 dark:hover:text-indigo-600 dark:focus:bg-neutral-700"
+         @click="viewAllOffers">
+        {{ showAllOffers ? 'Show less' : 'Show all' }}
+        <svg class="flex-shrink-0  w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <path d="m9 18 6-6-6-6"></path>
+        </svg>
+      </a>         
+    </div>
+  </div>
 </template>
 
 <script setup>
 
 import { onMounted, ref, computed } from 'vue';
-import { getDoc, doc, deleteDoc, getDocs } from 'firebase/firestore';
+import { getDoc, doc } from 'firebase/firestore';
 import { db } from '../main'; 
 import { useStore } from '../store/store';
 import emptyState from '../components/emptyState.vue';
 import classesCard from '../components/classesCard.vue';
+import offersCard from '../components/offersCard.vue';
 
 const store = useStore();
 const classes = ref([]);
 const showAllClasses = ref(false);
 const searchTerm = ref('');
-
+const uid = ref(store.docId);
+//Classes
 const displayedClasses = computed(() => {
   let filteredClasses = classes.value;
 
@@ -64,12 +95,12 @@ const displayedClasses = computed(() => {
   }
 });
 
-const viewAllProjects = () => {
+const viewAllClasses = () => {
   showAllClasses.value = !showAllClasses.value;
 };
 
 const fetchClasses = async () => {
-  if (!store.docId) {
+  if (!uid) {
     return;
   }
   const docRef = store.user.coach ? store.userDoc("coaches") : store.userDoc("users");
@@ -88,17 +119,75 @@ onMounted(async () => {
   fetchClasses();
 });
 
-const toggleModal = () => {
-  console.log('toggle modal')
+//Offers
+const offers = ref([]);
+const showAllOffers = ref(false);
+
+const displayedOffers = computed(() => {
+  let filteredOffers = offers.value;
+
+  if(searchTerm.value) {
+    filteredOffers = filteredOffers.filter(offer => 
+      offer.offerName.toLowerCase().includes(searchTerm.value.toLowerCase())
+    );
+  }
+  
+  if (showAllOffers.value) {
+    return filteredOffers;
+  } else {
+    return filteredOffers.slice(0, 3);
+  }
+});
+
+const viewAllOffers = () => {
+  showAllOffers.value = !showAllOffers.value;
+};
+
+ const fetchUserBookedEvents = async (db, uid) => {
+  const userRef = doc(db, "users", uid);
+  const userSnap = await getDoc(userRef);
+
+  if (userSnap.exists()) {
+    return userSnap.data().bookedEvents || [];
+  } else {
+    console.log("No such user!");
+    return [];
+  }
 }
 
-const deleteClass = async (uid) => {
-  const classRef = doc(db, "coaches", store.docId, "classes", uid);
-  try {
-    await deleteDoc(classRef);
-    fetchClasses();
-  } catch (error) {
-    console.error("Error deleting class: ", error);
-  }
-};
+const fetchOfferDetails = async (db, bookedEvents) => {
+  const offerDetailsPromises = bookedEvents.map(async (event) => {
+    const { bookedOffer, bookedCoach } = event;
+    const offerRef = doc(db, "coaches", bookedCoach, "Offers", bookedOffer);
+    const offerSnap = await getDoc(offerRef);
+    if (offerSnap.exists()) {
+      return { ...offerSnap.data(), bookedEvent: event };
+    } else {
+      console.log(`No such offer: ${bookedOffer} for coach: ${bookedCoach}`);
+      return null;
+    }
+  });
+
+  return Promise.all(offerDetailsPromises);
+}
+
+const getUserBookedOfferDetails = async (db, uid) => {
+  const bookedEvents = await fetchUserBookedEvents(db, uid);
+  const offerDetails = await fetchOfferDetails(db, bookedEvents);
+
+  return offerDetails.filter(details => details !== null); // Filter out any null results
+}
+
+
+
+onMounted(async () => {
+  // fetchOffers();
+  getUserBookedOfferDetails(db, uid.value)
+  .then(details => {
+    offers.value = details;
+  })
+  .catch(error => {
+    console.error("Error fetching offer details:", error);
+  });
+});
 </script>
--- src/views/schedule.vue
+++ src/views/schedule.vue
@@ -26,13 +26,13 @@
                     </svg>
                     My Profile
                   </router-link>
-                  <router-link class="py-4 px-1 inline-flex items-center gap-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-indigo-600 focus:outline-none focus:text-indigo-600 dark:text-neutral-500 dark:hover:text-indigo-500" to="/bookings">
+                  <router-link v-if="!store.user.coach" class="py-4 px-1 inline-flex items-center gap-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-indigo-600 focus:outline-none focus:text-indigo-600 dark:text-neutral-500 dark:hover:text-indigo-500" to="/bookings">
                     <svg class="flex-shrink-0 size-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 10h16M8 14h8m-4-7V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z"/>
                     </svg>
                     Bookings
                   </router-link>
-                  <router-link class="py-4 px-1 inline-flex items-center gap-2 border-b-2 border-transparent text-sm font-medium whitespace-nowrap text-gray-500 hover:text-indigo-600 focus:outline-none focus:text-indigo-800 dark:text-indigo-500" to="/connections">
+                  <router-link class="py-4 px-1 inline-flex items-center gap-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-indigo-600 focus:outline-none focus:text-indigo-800 dark:text-indigo-500" to="/connections">
                     <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                       <circle cx="12" cy="12" r="10"></circle>
                       <circle cx="12" cy="10" r="3"></circle>
