7176127be8d8a6f648a772b8868a4cad8e3d3529
Amine Oudjana <amine.oudjana@withsecure.com>
2025-04-04 20:21:15 +0200

    fix: add .env file

--- src/basic/const.js
+++ src/basic/const.js
@@ -1,2 +1,2 @@
-export const API_KEY = "AIzaSyDrvWDpSZHy-4tD48QQfirBJTA3yL9cHZ0";
+export const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 export const GOOGLE_API_SRC = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places,marker`;
